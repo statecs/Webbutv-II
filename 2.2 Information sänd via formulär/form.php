8b2b7e86a703b19ed23f9b6506b3fd4767df7722
Christopher State <statec@icloud.com>
2021-10-17 14:55:00 +0200

    2.2

--- 2.2 Information sänd via formulär/form.php
+++ 2.2 Information sänd via formulär/form.php
@@ -2,8 +2,8 @@
 header('Content-type: text/plain');
 echo "\n";
 
-//Skriver ut nycklar med värde som skickas in (_GET)
-foreach($_GET as $key => $value){
+//Skriver ut nycklar med värde som skickas in (_POST)
+foreach($_POST as $key => $value){
 	echo $key . " = " . $value . "\n";
   }
 
